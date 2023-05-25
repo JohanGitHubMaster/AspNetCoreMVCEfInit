8cfa751a8f128a6086e3f1f73cbbc3f4059d4e59
JohanGitHubMaster <rakotovaojohan516@gmail.com>
2023-05-25 15:52:18 +0300

    ajout CRUD

---
+++ MvcMovie/Models/CrudMovie.cs
@@ -0,0 +1,14 @@
+﻿using MvcMovie.Data;
+
+namespace MvcMovie.Models
+{
+    public class CrudMovie
+    {
+        public CrudMovie() { }
+
+        public async static Task SaveContext(MvcMovieContext context)
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
